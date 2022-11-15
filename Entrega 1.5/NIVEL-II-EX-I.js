7c0b55888e3a973d6d4b629e217dc086e3545b54
elizabeth <emendoza867@gmail.com>
2022-11-15 12:46:33 +0100

    funcion recursiva que muestre un mensaje por consola y demore 1 segundo

--- Entrega 1.5/NIVEL-II-EX-I.js
+++ Entrega 1.5/NIVEL-II-EX-I.js
@@ -1,3 +1,19 @@
 /*- Exercici 1
 Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.*/
 
+function countDown(fromNumber) {
+   setTimeout(()=> console.log(fromNumber),3000);
+
+    let nextNumber = fromNumber - 1;
+
+    if (nextNumber > 0) {
+        countDown(nextNumber);
+    }
+}
+countDown(3);
+
+ 
+
+/********************************************************** */
+
+ 
