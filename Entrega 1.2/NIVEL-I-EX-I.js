b9b04a7cb0cd0ac47933a89b81cd3305a65a03c0
Elizabeth Mendoza <emendoza867@gmail.com>
2022-11-17 10:41:31 +0100

    Update NIVEL-I-EX-I.js

--- Entrega 1.2/NIVEL-I-EX-I.js
+++ Entrega 1.2/NIVEL-I-EX-I.js
@@ -2,6 +2,17 @@
 - Exercici 1
 Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.*/
 
-( (nombre,nombre2) => {
+/*( (nombre,nombre2) => {
     console.log(nombre+nombre2);
-  } )  ("María","Jose");
+  } )  ("María","Jose");*/
+
+/*Nivell 2
+- Exercici 1
+Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut. ===> CORREGIDO! */
+
+const obtenerUser = user => ({ name: user.name});
+
+const user = { name: "Elizabeth", color: "purple" };
+
+console.log(obtenerUser(user));
+ 
