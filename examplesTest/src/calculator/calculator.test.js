72532f076f24d8104f9f51c6e924eb94e6ad6a27
ELizabeth <emendoza867@gmail.com>
2022-11-21 12:49:00 +0100

    cambios nuevos

--- examplesTest/src/calculator/calculator.test.js
+++ examplesTest/src/calculator/calculator.test.js
@@ -1,8 +1,8 @@
 const calculator = require(".");
-const calculator= require (".");
+ 
 test("sum calculator",()=> {
     const calculator= calculator.sum(1,2);
-    expect(result).toBe(3);
+    expect(result).toBe(4);
    //
 });
 test.todo("extract calculator");
