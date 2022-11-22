fff8f997fdf54924775fce809a4624cc30438516
Elizabeth <emendoza867@gmail.com>
2022-11-22 12:34:07 +0100

    Testting de validacion de email

---
+++ examplesTest/validaemail.test.js
@@ -0,0 +1,11 @@
+const ValidaEmail = require('./validaemail')
+
+test('email emendoza867@gmail.com -> true',() =>{
+    expect(ValidaEmail.isValidEmail('emendoza867@gmail.com')).toBe(true)
+
+});
+
+test('email 1 -> false',() =>{
+    expect(ValidaEmail.isValidEmail('1')).toBe(true)
+
+});
